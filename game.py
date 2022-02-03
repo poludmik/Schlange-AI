2c3b12e227b47785050d2d820696fcca270d0e65
poludmik <poludmik@fel.cvut.cz>
2022-02-03 14:16:45 +0100

    Code refactoring, split into files.

--- game.py
+++ game.py
@@ -1,9 +1,8 @@
-import random
-
 import pygame
 import math
-from random import randint
+import random
 from tqdm import tqdm
+import snake
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -18,8 +17,8 @@ STRAIGHT = [1, 0, 0]
 
 class SnakeGame:
 
-    def __init__(self, size, dark=True):
-        self.window_size = 400
+    def __init__(self, size, dark=True, window_size=400):
+        self.window_size = window_size
         self.margin = 1
         self.width = size
         self.height = size
@@ -151,7 +150,7 @@ class SnakeGame:
         if self.current_snake:
             del self.current_snake
 
-        self.current_snake = Snake(self)
+        self.current_snake = snake.Snake(self)
         self.current_snake.draw_snake()
         textsurface = self.myfont.render(str(len(self.current_snake.rest_of_body_positions)), False, self.tile_color)
         self.screen.blit(textsurface, (10, self.window_size))
@@ -202,107 +201,11 @@ class SnakeGame:
             print("No game was running to stop it.")
 
 
-class Snake:
-
-    def __init__(self, game_local):
-        self.head_position = self.generate_random_starting_point()
-        self.head_direction = random.choice([[1, 0], [0, -1], [-1, 0], [0, 1]])
-        self.rest_of_body_positions=[]
-        self.length = 1
-
-    def __del__(self):
-        #print("Deleted snake")
-        pass
-
-    def generate_random_starting_point(self):
-        startin_point = (random.randint(1, game.width - 2), random.randint(1, game.height - 2))
-        while startin_point == game.food_position:
-            startin_point = (random.randint(1, game.width - 2), random.randint(1, game.height - 2))
-        return startin_point
-
-    def make_step_by_given_action(self, action): # action is in form of [0-1, 0-1, 0-1]
-        reward = -0.01
-
-        new_head_direction = self.rotate_head_direction(self.head_direction, action)
-
-        next_tile = (self.head_position[0] + new_head_direction[1], self.head_position[1] + new_head_direction[0])
-
-        if next_tile[0] < 0 or next_tile[0] >= game.width or next_tile[1] < 0 or next_tile[1] >= game.height:
-            #print("Wall collision")
-            reward = -10
-            game.set_color_to_one_cell(self.head_position[0], self.head_position[1], RED)
-            game.clock.tick(5)
-            pygame.display.flip()
-            game.game_running = False
-            return reward
-
-        elif (next_tile[0], next_tile[1]) in self.rest_of_body_positions:
-            #print("Body collision")
-            reward = -10
-            game.set_color_to_one_cell(self.head_position[0], self.head_position[1], RED)
-            game.clock.tick(5)
-            pygame.display.flip()
-            game.game_running = False
-            return reward
-
-        elif next_tile == game.food_position:
-            # found food
-            reward = 10
-            game.set_color_to_one_cell(game.food_position[0], game.food_position[1], game.tile_color)
-
-            # random food coordinates
-            game.food_position = (random.randint(0, game.width-1), random.randint(0, game.height-1))
-            while game.food_position in self.rest_of_body_positions\
-                    or game.food_position == self.head_position\
-                    or game.food_position == next_tile:
-                game.food_position = (random.randint(0, game.width-1), random.randint(0, game.height-1))
-
-            game.set_color_to_one_cell(game.food_position[0], game.food_position[1], GREEN) # draw new food
-
-        # move snake
-        if reward != 10 and len(self.rest_of_body_positions): # if not growing this step, delete tail
-            game.set_color_to_one_cell(self.rest_of_body_positions[0][0], self.rest_of_body_positions[0][1],
-                                       game.tile_color)
-            self.rest_of_body_positions.pop(0)
-            self.rest_of_body_positions.append(self.head_position)
-        if reward != 10 and len(self.rest_of_body_positions) == 0:
-            game.set_color_to_one_cell(self.head_position[0], self.head_position[1],
-                                       game.tile_color)
-        if reward == 10:
-            self.rest_of_body_positions.append(self.head_position)
-
-        self.head_position = next_tile
-        self.head_direction = new_head_direction
-        self.draw_snake()
-
-        return reward
-
-    def draw_snake(self):
-        game.set_color_to_one_cell(self.head_position[0], self.head_position[1], STRONGER_BLUE)
-        for body_fragment in self.rest_of_body_positions:
-            game.set_color_to_one_cell(body_fragment[0], body_fragment[1], PALE_BLUE)
-        game.draw_direction(self.head_position[0], self.head_position[1], self.head_direction)
-
-        game.clock.tick(1000)
-        textsurface = game.myfont.render(str(len(game.current_snake.rest_of_body_positions)), False, game.tile_color)
-        pygame.draw.rect(game.screen, game.margin_color, [0, game.window_size, game.window_size, 30])
-        game.screen.blit(textsurface, (10, game.window_size))
-
-        pygame.display.flip()
-
-    def rotate_head_direction(self, vector, where):
-        if where == LEFT:    # -pi/2
-            return [vector[1], -vector[0]]
-        elif where == RIGHT: # pi/2
-            return [-vector[1], vector[0]]
-        else:                # no change
-            return vector
-
 
 if __name__ == "__main__":
     print("Starting gaming")
 
-    game = SnakeGame(8, True)
+    game = SnakeGame(8, dark=True, window_size=400)
 
     play_on_keyboard = False
 
@@ -310,7 +213,7 @@ if __name__ == "__main__":
         game.play_games()
     else:
         # for learning, i.e.
-        for _ in tqdm(range(100), position=0, leave=True):
+        for _ in tqdm(range(30), position=0, leave=True):
             game.ensure_game_is_running()
             # get state
             # decide
---
+++ snake.py
@@ -0,0 +1,116 @@
+import pygame
+import math
+import random
+from tqdm import tqdm
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (102, 204, 0)
+RED = (255, 0, 0)
+PALE_BLUE = (102, 178, 255)
+STRONGER_BLUE = (102, 102, 255)
+DARK_BLUE = (0, 25, 51)
+LEFT = [0, 1, 0]
+RIGHT = [0, 0, 1]
+STRAIGHT = [1, 0, 0]
+
+
+class Snake:
+    def __init__(self, game):
+        self.game = game
+        self.head_position = self.generate_random_starting_point()
+        self.head_direction = random.choice([[1, 0], [0, -1], [-1, 0], [0, 1]])
+        self.rest_of_body_positions=[]
+        self.length = 1
+
+    def __del__(self):
+        #print("Deleted snake")
+        pass
+
+    def generate_random_starting_point(self):
+        startin_point = (random.randint(1, self.game.width - 2), random.randint(1, self.game.height - 2))
+        while startin_point == self.game.food_position:
+            startin_point = (random.randint(1, self.game.width - 2), random.randint(1, self.game.height - 2))
+        return startin_point
+
+    def make_step_by_given_action(self, action): # action is in form of [0-1, 0-1, 0-1]
+        reward = -0.01
+
+        new_head_direction = self.rotate_head_direction(self.head_direction, action)
+
+        next_tile = (self.head_position[0] + new_head_direction[1], self.head_position[1] + new_head_direction[0])
+
+        if next_tile[0] < 0 or next_tile[0] >= self.game.width or next_tile[1] < 0 or next_tile[1] >= self.game.height:
+            #print("Wall collision")
+            reward = -10
+            self.game.set_color_to_one_cell(self.head_position[0], self.head_position[1], RED)
+            self.game.clock.tick(5)
+            pygame.display.flip()
+            self.game.game_running = False
+            return reward
+
+        elif (next_tile[0], next_tile[1]) in self.rest_of_body_positions:
+            #print("Body collision")
+            reward = -10
+            self.game.set_color_to_one_cell(self.head_position[0], self.head_position[1], RED)
+            self.game.clock.tick(5)
+            pygame.display.flip()
+            self.game.game_running = False
+            return reward
+
+        elif next_tile == self.game.food_position:
+            # found food
+            reward = 10
+            self.game.set_color_to_one_cell(self.game.food_position[0], self.game.food_position[1], self.game.tile_color)
+
+            # random food coordinates
+            self.game.food_position = (random.randint(0, self.game.width-1), random.randint(0, self.game.height-1))
+            while self.game.food_position in self.rest_of_body_positions\
+                    or self.game.food_position == self.head_position\
+                    or self.game.food_position == next_tile:
+                self.game.food_position = (random.randint(0, self.game.width-1), random.randint(0, self.game.height-1))
+
+            self.game.set_color_to_one_cell(self.game.food_position[0], self.game.food_position[1], GREEN) # draw new food
+
+        # move snake
+        if reward != 10 and len(self.rest_of_body_positions): # if not growing this step, delete tail
+            self.game.set_color_to_one_cell(self.rest_of_body_positions[0][0], self.rest_of_body_positions[0][1],
+                                       self.game.tile_color)
+            self.rest_of_body_positions.pop(0)
+            self.rest_of_body_positions.append(self.head_position)
+        if reward != 10 and len(self.rest_of_body_positions) == 0:
+            self.game.set_color_to_one_cell(self.head_position[0], self.head_position[1],
+                                       self.game.tile_color)
+        if reward == 10:
+            self.rest_of_body_positions.append(self.head_position)
+
+        self.head_position = next_tile
+        self.head_direction = new_head_direction
+        self.draw_snake()
+
+        return reward
+
+    def draw_snake(self):
+        self.game.set_color_to_one_cell(self.head_position[0], self.head_position[1], STRONGER_BLUE)
+
+        self.game.clock.tick(1000)
+
+        # Draw snake fragments
+        for body_fragment in self.rest_of_body_positions:
+            self.game.set_color_to_one_cell(body_fragment[0], body_fragment[1], PALE_BLUE)
+        self.game.draw_direction(self.head_position[0], self.head_position[1], self.head_direction)
+
+        # Show snake score
+        textsurface = self.game.myfont.render(str(len(self.game.current_snake.rest_of_body_positions)), False, self.game.tile_color)
+        pygame.draw.rect(self.game.screen, self.game.margin_color, [0, self.game.window_size, self.game.window_size, 30])
+        self.game.screen.blit(textsurface, (10, self.game.window_size))
+
+        pygame.display.flip()
+
+    def rotate_head_direction(self, vector, where):
+        if where == LEFT:    # -pi/2
+            return [vector[1], -vector[0]]
+        elif where == RIGHT: # pi/2
+            return [-vector[1], vector[0]]
+        else:                # no change
+            return vector
